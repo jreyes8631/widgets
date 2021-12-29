22ad2513e4bc9e6b7bdeb413ab35d66fc90ad18b
jreyes8631 <jreyes8631@gmail.com>
2021-12-29 16:35:45 -0500

    refactoring

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Accordion = ({items}) => {
     
    const rendereditems = items.map(item =>{
-        return <div key={item.title}>
+        return (
+        <React.Fragment key={item.title}>
              <div className="title active">
                  <i className="dropdown icon"></i>
                  {item.title}
@@ -11,7 +12,8 @@ const Accordion = ({items}) => {
              <div className="content active">
                  <p>{item.conent}</p>
              </div>
-        </div>
+        </React.Fragment>
+        );
     })
        
     return <div className="ui styled accordion">
