907544a41accfee22b274f29d27e3fa0711416d6
jreyes8631 <jreyes8631@gmail.com>
2021-12-29 15:28:08 -0500

    mapping through items and adding style

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const Accordion = ({items}) => {
+    
+    rendereditems = items.map(item =>{
+        return <div>
+             <div className="title active">
+                 <i className="dropdown icon"></i>
+                 {item.title}
+             </div>
+             <div className="content active">
+                 <p>{item.conent}</p>
+             </div>
+        </div>
+    })
+       
     return <h1> there are {items.length} items</h1>
 
 }
