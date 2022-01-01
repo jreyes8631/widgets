fb8eb476642cefe86e910aad084cc21e6e42b4a8
jreyes8631 <jreyes8631@gmail.com>
2021-12-31 22:36:54 -0500

    using useState to change the state of a functional component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({items}) => {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+       setActiveIndex(index)
+    }
     
    const rendereditems = items.map((item, index) => {
         return (
         <React.Fragment key={item.title}>
              <div 
               className="title active"
-              onClick={() => console.log('title clicked', index)}
+              onClick={() => onTitleClick(index)}
               >
                 <i className="dropdown icon"></i>
                 {item.title}
@@ -21,6 +27,7 @@ const Accordion = ({items}) => {
        
     return <div className="ui styled accordion">
         {rendereditems}
+        <h1>{activeIndex}</h1>
     </div>
 }
 
